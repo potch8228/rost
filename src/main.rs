04a61f56d90e7f87a70fad68905d33eadc350519
Tatsuya Kobayashi <pikopiko28@gmail.com>
2026-04-01 14:45:58 +0900

    ch3. add allocator test, add init

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,18 @@ use core::writeln;
 use rost::graphics::draw_test_pattern;
 use rost::graphics::fill_rect;
 use rost::graphics::Bitmap;
+use rost::init::init_basic_runtime;
 use rost::qemu::exit_qemu;
 use rost::qemu::QemuExitCode;
-use rost::serial::SerialPort;
-use rost::uefi::exit_from_efi_boot_services;
 use rost::uefi::init_vram;
 use rost::uefi::EfiHandle;
 use rost::uefi::EfiMemoryType;
 use rost::uefi::EfiSystemTable;
-use rost::uefi::MemoryMapHolder;
 use rost::uefi::VramTextWriter;
-
 use rost::x86::hlt;
 
 #[no_mangle]
 fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    let mut sw = SerialPort::new_for_com1();
-    writeln!(sw, "Hello via serial port").unwrap();
     let mut vram = init_vram(efi_system_table).expect("init_vram failed");
     let vw = vram.width();
     let vh = vram.height();
@@ -33,15 +28,7 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     draw_test_pattern(&mut vram);
 
     let mut w = VramTextWriter::new(&mut vram);
-    for i in 0..4 {
-        writeln!(w, "i = {i}").unwrap();
-    }
-
-    let mut memory_map = MemoryMapHolder::new();
-    let status = efi_system_table
-        .boot_services()
-        .get_memory_map(&mut memory_map);
-    writeln!(w, "{status:?}").unwrap();
+    let memory_map = init_basic_runtime(image_handle, efi_system_table);
     let mut total_memory_pages = 0;
     for e in memory_map.iter() {
         if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
@@ -58,7 +45,6 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     )
     .unwrap();
 
-    exit_from_efi_boot_services(image_handle, efi_system_table, &mut memory_map);
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
 
     loop {
