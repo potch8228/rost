1a0bc29503556e63d93e9ae6ef68dd9bba9b2373
Tatsuya Kobayashi <pikopiko28@gmail.com>
2026-04-05 16:05:08 +0900

    ch.6 get USB Endpoint/InterfaceDescriptor

--- src/xhci.rs
+++ src/xhci.rs
@@ -209,6 +209,9 @@ impl PciXhciDriver {
                     None
                 };
                 info!("xhci: v/p/s = {vendor:?}/{product:?}/{serial:?}");
+                let descriptors =
+                    Self::request_config_descriptor_and_rest(&xhc, slot, &mut ctrl_ep_ring).await?;
+                info!("xhci: {descriptors:?}");
             }
         }
         Ok(())
@@ -1767,9 +1770,9 @@ impl StatusStageTrb {
 #[derive(Debug, Copy, Clone)]
 pub enum UsbDescriptor {
     Config(ConfigDescriptor),
-    /*
     Endpoint(EndpointDescriptor),
     Interface(InterfaceDescriptor),
+    /*
     Hid(HidDescriptor),
     */
     Unknown { desc_len: u8, desc_type: u8 },
@@ -1824,13 +1827,13 @@ impl<'a> Iterator for DescriptorIterator<'a> {
                 e if e == UsbDescriptorType::Config as u8 => {
                     UsbDescriptor::Config(ConfigDescriptor::copy_from_slice(buf).ok()?)
                 }
-                /*
                 e if e == UsbDescriptorType::Interface as u8 => {
                     UsbDescriptor::Interface(InterfaceDescriptor::copy_from_slice(buf).ok()?)
                 }
                 e if e == UsbDescriptorType::Endpoint as u8 => {
                     UsbDescriptor::Endpoint(EndpointDescriptor::copy_from_slice(buf).ok()?)
                 }
+                /*
                 e if e == UsbDescriptorType::Hid as u8 => {
                     UsbDescriptor::Hid(HidDescriptor::copy_from_slice(buf).ok()?)
                 }
@@ -1845,3 +1848,60 @@ impl<'a> Iterator for DescriptorIterator<'a> {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone, Default)]
+#[allow(unused)]
+#[repr(packed)]
+pub struct InterfaceDescriptor {
+    desc_length: u8,
+    desc_type: u8,
+    pub interface_number: u8,
+    pub alt_setting: u8,
+    num_of_endpoints: u8,
+    interface_class: u8,
+    interface_subclass: u8,
+    interface_protocol: u8,
+    interface_index: u8,
+}
+const _: () = assert!(size_of::<InterfaceDescriptor>() == 9);
+unsafe impl IntoPinnedMutableSlice for InterfaceDescriptor {}
+unsafe impl Sliceable for InterfaceDescriptor {}
+/*
+impl InterfaceDescriptor {
+    pub fn triple(&self) -> (u8, u8, u8) {
+        (
+            self.interface_class,
+            self.interface_subclass,
+            self.interface_protocol,
+        )
+    }
+}
+*/
+
+#[derive(Debug, Copy, Clone, Default)]
+#[allow(unused)]
+#[repr(packed)]
+pub struct EndpointDescriptor {
+    pub desc_length: u8,
+    pub desc_type: u8,
+
+    // endpoint_address:
+    //   - bit[0..=3]: endpoint number
+    //   - bit[7]: direction(0: out, 1: in)
+    pub endpoint_address: u8,
+
+    // attributes:
+    //   - bit[0..=1]: transfer type(0: Control, 1: Isochronous, 2: Bulk, 3:
+    //     Interrupt)
+    pub attributes: u8,
+    pub max_packet_size: u16,
+    // interval:
+    // [xhci] Table 6-12
+    // interval_ms = interval (For FS/LS Interrupt)
+    // interval_ms = 2^(interval-1) (For FS Isoch)
+    // interval_ms = 2^(interval-1) (For SSP/SS/HS)
+    pub interval: u8,
+}
+const _: () = assert!(size_of::<EndpointDescriptor>() == 7);
+unsafe impl IntoPinnedMutableSlice for EndpointDescriptor {}
+unsafe impl Sliceable for EndpointDescriptor {}
